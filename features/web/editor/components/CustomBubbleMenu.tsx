a5ac87810587fb09eaa3f8b32a80ad7fab2355c6
Matheus Cristian <matheuscristianrg@gmail.com>
2025-07-01 20:55:24 -0300

    Adiciona opções de underline e listas ao context menu

--- features/web/editor/components/CustomBubbleMenu.tsx
+++ features/web/editor/components/CustomBubbleMenu.tsx
@@ -1,12 +1,21 @@
 import cn from "@/libs/utils/cn";
 import { useCurrentEditor } from "@tiptap/react";
-import { Bold, Italic, Strikethrough } from "lucide-react";
+import {
+    Bold,
+    Italic,
+    List,
+    ListOrdered,
+    Strikethrough,
+    Underline,
+} from "lucide-react";
+
+const iconSize = 15;
 
 export default function CustomBubbleMenu() {
     const editor = useCurrentEditor();
 
     return (
-        <div className="bg-border rounded-md flex [&>div]:cursor-pointer [&>div]:first:rounded-l-md [&>div]:last:rounded-r-md [&>div]:hover:bg-accent-primary/5 [&>div]:py-2 [&>div]:px-3">
+        <div className="bg-border rounded-md flex items-center justify-center [&>div]:cursor-pointer [&>div]:first:rounded-l-md [&>div]:last:rounded-r-md [&>div]:hover:bg-accent-primary/5 [&>div]:p-2">
             <div
                 className={cn(
                     "p-3",
@@ -16,7 +25,7 @@ export default function CustomBubbleMenu() {
                     editor.editor?.chain().focus().toggleBold().run()
                 }
             >
-                <Bold size={12} />
+                <Bold size={iconSize} />
             </div>
             <div
                 className={cn(
@@ -27,7 +36,7 @@ export default function CustomBubbleMenu() {
                     editor.editor?.chain().focus().toggleItalic().run()
                 }
             >
-                <Italic size={12} />
+                <Italic size={iconSize} />
             </div>
             <div
                 className={cn(
@@ -38,7 +47,44 @@ export default function CustomBubbleMenu() {
                     editor.editor?.chain().focus().toggleStrike().run()
                 }
             >
-                <Strikethrough size={12} />
+                <Strikethrough size={iconSize} />
+            </div>
+            <div
+                className={cn(
+                    "p-3",
+                    editor.editor?.isActive("underline") &&
+                        "!bg-accent-focus/15",
+                )}
+                onClick={() =>
+                    editor.editor?.chain().focus().toggleUnderline().run()
+                }
+            >
+                <Underline size={iconSize} />
+            </div>
+            <hr className="h-[25px] w-[1px] border-0 bg-white/10" />
+            <div
+                className={cn(
+                    "p-3",
+                    editor.editor?.isActive("bulletList") &&
+                        "!bg-accent-focus/15",
+                )}
+                onClick={() =>
+                    editor.editor?.chain().focus().toggleBulletList().run()
+                }
+            >
+                <List size={iconSize} />
+            </div>
+            <div
+                className={cn(
+                    "p-3",
+                    editor.editor?.isActive("orderedList") &&
+                        "!bg-accent-focus/15",
+                )}
+                onClick={() =>
+                    editor.editor?.chain().focus().toggleOrderedList().run()
+                }
+            >
+                <ListOrdered size={iconSize} />
             </div>
         </div>
     );
