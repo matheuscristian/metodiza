071a3c38cfcddc2fc6c4da28c7afcd7f0b61a1bb
Matheus Cristian <matheuscristianrg@gmail.com>
2025-08-02 16:22:35 -0300

    Refaz páginas de cadastro e login, implementa funcionalidades, bloqueia /web/* se não estiver logado (fixes #40)

--- app/web/layout.tsx
+++ app/web/layout.tsx
@@ -1,10 +1,18 @@
 import SideBar from "@/features/web/components/sidebar";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function WebAppLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const token = (await cookies()).get("__session");
+
+    if (!token) {
+        redirect("/join");
+    }
+
     return (
         <div className="flex max-h-screen max-w-screen overflow-hidden">
             <SideBar />
